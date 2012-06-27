21dea299a21131e2265ef98a99ae6179e231b077
Hugues Obolonsky <hugh@atosc.org>
2012-06-28 00:48:49 +0200

    Add stylesheet js and csrf_meta in layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "StaticPages" do
   
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_content('You are on the Rails Tutorial')
     end
 
     it "should have the right title" do
@@ -27,7 +27,7 @@ describe "StaticPages" do
   
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_content('Get help on the Ruby')
     end
 
     it "should have the title 'Help'" do
@@ -40,7 +40,7 @@ describe "StaticPages" do
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_content('We are legion')
     end
 
     it "should have the title 'About'" do
