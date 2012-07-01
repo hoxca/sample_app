2e3659dbedd9a9938ba5391d8a39861a1e3296d2
Hugues Obolonsky <hugh@atosc.org>
2012-07-01 14:57:38 +0200

    more concise before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX } 
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
 end
