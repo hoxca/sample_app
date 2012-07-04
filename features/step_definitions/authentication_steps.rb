5f2f3e55249cbad2f17393524502fdd1f61f8640
Hugues Obolonsky <hugh@atosc.org>
2012-07-04 02:36:56 +0200

    fix cucumber authentication steps

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,9 +1,9 @@
 Given /^a user visits the signin page$/ do
-    visit signin_path
+  visit signin_path
 end
 
 When /^he submits invalid signin information$/ do
-    click_button "Sign in"
+  click_button "Sign in"
 end
 
 Then /^he should see an error message$/ do
@@ -11,21 +11,25 @@ Then /^he should see an error message$/ do
 end
 
 Given /^the user has an account$/ do
-    @user = User.create(name: "Example User", email: "user@example.com",
-                                             password: "foobar", password_confirmation: "foobar")
+  @user = User.create(common_name: "Example User",
+                      nickname: "User nick",
+                      first_name: "Example",
+                      last_name: "User",
+                      email: "user@example.com",
+                      password: "foobar", password_confirmation: "foobar")
 end
 
 When /^the user submits valid signin information$/ do
-    fill_in "Email",    with: @user.email
-      fill_in "Password", with: @user.password 
-        click_button "Sign in"
+  fill_in "Email",    with: @user.email
+  fill_in "Password", with: @user.password 
+  click_button "Sign in"
 end
 
 Then /^he should see his profile page$/ do
-    page.should have_selector('title', text: @user.name)
+  page.should have_selector('title', text: @user.nickname)
 end
 
 Then /^he should see a signout link$/ do
-    page.should have_link('Sign out', href: signout_path)
+  page.should have_link('Sign out', href: signout_path)
 end
 
